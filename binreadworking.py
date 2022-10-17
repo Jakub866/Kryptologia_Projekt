db18d15073520f91a2abeaace39e7acd40c8ac4e
Jakub Grzesiak <s9208@edukacja.awl.edu.pl>
2022-10-17 20:32:11 +0200

    Now wokring function saving_decrypt().

--- binreadworking.py
+++ binreadworking.py
@@ -7,8 +7,9 @@ def saving_encrypting(choose_encryption_method: int, src_f: str):
     index_key = "".join((chr(i) for i in range(128)))
     ces_shift = "t"
     OriginalFile = open(src_f, "rb")
-    EncryptedFile = open("Encrypted_" + src_f, "w")
+    EncryptedFile = open("Encrypted", "w")
     while (byte := OriginalFile.read(1)):
+        #print(byte)
         non_crypted_double_bytes = bytes.hex(byte)
         for non_crypted_single_bytes in non_crypted_double_bytes:
             match choose_encryption_method:
@@ -17,21 +18,20 @@ def saving_encrypting(choose_encryption_method: int, src_f: str):
                         print("Przesunięcie: ", end='')
                         ces_shift = int(input())
                     EncryptedFile.write(str(index_key.index(cz.enc_ceasar(ces_shift,non_crypted_single_bytes))))
-                case 2:                            
-                    EncryptedFile.write(str(index_key.index(atbasz.toAtBash(non_crypted_single_bytes))))
+                case 2:         
+                    EncryptedFile.write(str(index_key.index(atbasz.toAtBash(non_crypted_single_bytes))))                
                 case default:
                     print("Encryption method not found")
                     quit()
-            #print(index_key.index(enc_ceasar(32,z)))
-            
-            #writer.write(str(index_key.index(atbasz.toAtBash(z))))
             EncryptedFile.write("\n")
     OriginalFile.close()
     EncryptedFile.close()
 
-saving_encrypting(2, "Source.jpg")
+saving_encrypting(1, "Source.jpg")
+
+index_key = "".join((chr(i) for i in range(128)))
 sending_bytes =[]
-with open("Encrypred_" + "Source.jpg", "r") as reader:
+with open("Encrypted", "r") as reader:
     while (byte := reader.readline()):
         byte = byte.strip()
         #print(byte)
@@ -43,8 +43,8 @@ with open("Encrypred_" + "Source.jpg", "r") as reader:
         for x in sending_bytes:
             #print(x, "->", index_key[int(x)], "<-", dec_ceasar(32, index_key[int(x)]))
             x =  index_key[int(x)]
-            #x = cz.dec_ceasar(32, x)
-            x = atbasz.toAtBash(x)
+            x = cz.dec_ceasar(32, x)
+            #x = atbasz.toAtBash(x)
             #print(x)
             if moze:
                 wynik = wynik + x
