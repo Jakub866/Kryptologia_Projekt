2f6c154b87e19ce5f6f111c21eecde32de062f00
IgorMielczarek <100940274+BaldwinSecundo@users.noreply.github.com>
2022-12-01 21:58:26 +0100

    Help button and encryption/decryption via path

--- binread.py
+++ binread.py
@@ -1,27 +1,81 @@
-import cezar
+import binascii
+import algorithms.ciphers.cezar as cz
+import algorithms.ciphers.atbash as atbasz
+import algorithms.ciphers.vigenere as vig
+import algorithms.ciphers.substitution as sub
+
+index_key = "".join((chr(i) for i in range(128)))
+
+def saving_encrypting(choose_encryption_method: int, src_f: str, src_s:str, key):
+    print(choose_encryption_method)
+    OriginalFile = open(src_f, "rb")
+    EncryptedFile = open(src_s, "w")
+    while (byte := OriginalFile.read(1)):
+        non_crypted_double_bytes = bytes.hex(byte)
+        for non_crypted_single_bytes in non_crypted_double_bytes:
+            match choose_encryption_method:
+                case 1:
+                    EncryptedFile.write(str(index_key.index(cz.enc_ceasar(int(key),non_crypted_single_bytes))))
+                case 2:
+                    EncryptedFile.write(str(index_key.index(atbasz.toAtBash(non_crypted_single_bytes))))
+                case 3:
+                    EncryptedFile.write((str(index_key.index(vig.encryption(key,non_crypted_single_bytes)))))
+                case 4:
+                    EncryptedFile.write(str(index_key.index(sub.enc_substit(key,non_crypted_single_bytes))))
+                case default:
+                    pass
+            EncryptedFile.write("\n")
+    OriginalFile.close()
+    EncryptedFile.close()
 
 
-OriginalFile = open("Testowy.jpg", "rb")
-EncryptedFile = open("EncryptedTestowy.jpg", "w")
-while (byte := OriginalFile.read(1)):
-    LengthOfByte = len(str(byte)) - 1
-    EncryptedByte = cezar.enc_ceasar(5, str(byte)[2:LengthOfByte])
-    EncryptedByte = EncryptedByte + "\n"
-    EncryptedFile.write(EncryptedByte)
+def reading_decrypting(choose_encryption_method: int, src_s: str, src_f:str, key: int):
+    EncrypredFile = open(src_s, "r")
+    DecryptedFile = open(src_f, "wb")
+    wynik = ""
+    while (byte := EncrypredFile.readline()):
+        byte = byte.strip()
+        byte =  index_key[int(byte)]
+        match choose_encryption_method:
+                case 1:
+                    byte = cz.dec_ceasar(key, byte)
+                case 2:
+                    byte = atbasz.toAtBash(byte)
+                case 3:
+                    byte = vig.decryption(key,byte)
+                case 4:
+                    byte = sub.dec_substit(key,byte)
+                case default:
+                    pass
+        wynik = wynik + byte
+        if len(wynik) == 2:
+            DecryptedFile.write(binascii.unhexlify(wynik))
+            wynik = ''
+    DecryptedFile.close()
+    EncrypredFile.close()
 
-OriginalFile.close()
-EncryptedFile.close()
+def handle_input_enc(choose_encryption_method: int, inp, key):
+    match choose_encryption_method:
+        case 1:
+            return cz.enc_ceasar(int(key),inp)
+        case 2:
+            return atbasz.toAtBash(inp)
+        case 3:
+            return vig.encryption(key,inp)
+        case 4:
+            return sub.enc_substit(key,inp)
+        case default:
+            pass
 
-File2Decrypt = open("EncryptedTestowy.jpg", "r")
-DecryptedFile = open("WynikTestowy.jpg", "wb")
-
-while (line := File2Decrypt.readline()):
-    LengthOfByte = len(str(line)) - 1
-    DecryptedByte = cezar.dec_ceasar(5, str(line))
-    if DecryptedByte[LengthOfByte].encode("UTF-8") == b'\x05':
-        DecryptedByte = DecryptedByte[:LengthOfByte]
-    if len(DecryptedByte) == 3:
-        ToWrite = DecryptedByte.encode()
-    else:
-        ToWrite = DecryptedByte.encode().decode('unicode_escape').encode("raw_unicode_escape")
-    DecryptedFile.write(ToWrite)
+def handle_input_dec(choose_encryption_method: int, inp, key):
+    match choose_encryption_method:
+        case 1:
+            return cz.dec_ceasar(int(key),inp)
+        case 2:
+            return atbasz.toAtBash(inp)
+        case 3:
+            return vig.decryption(key,inp)
+        case 4:
+            return sub.dec_substit(key,inp)
+        case default:
+            pass
--- main_page.py
+++ main_page.py
@@ -4,165 +4,121 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
 from kivy.uix.button import Button
 from plyer import filechooser
-from kivy.properties import ListProperty
-
-import algorithms.ciphers.cezar as cezar
-import algorithms.ciphers.atbash as atbash
-import algorithms.ciphers.vigenere as vigenere
-import algorithms.ciphers.substitution as substitution
-
+import binread
+from kivy.animation import Animation
+from kivy.clock import Clock, mainthread
 # #005776 hex of color blue
 
 
 class FileChoose(Button):
-    selection = ListProperty([])
-
     def __init__(self, **kwargs):
         super(FileChoose,self).__init__(**kwargs)
 
     def choose(self,*args):
         return filechooser.open_file()
 
+    def save(self,*args):
+        return filechooser.save_file()
 
-    def on_selection(self, *a, **k):
-        global path
-        path = str(self.selection).strip().strip("[").strip("]").strip("'")
 
 
 class WindowManager(ScreenManager):
     pass
 
 
+class Page(Screen):
+
+    def fade(self,dt):
+        anim = Animation(opacity=0, duration=2)
+        anim.start(self.ids.status)
+
+
+
+    def status(self,status):
+        self.ids.status.opacity = 1
+        self.ids.status.text = status
+        Clock.schedule_once(self.fade,10)
+
+
+    def refresh(self):
+        ids_list = [self.ids.loadDirectory,self.ids.saveDirectory,
+                    self.ids.keyphrase, self.ids.input, self.ids.output, self.ids.status]
+        for i in ids_list:
+            i.text = " "
+
+    def set_load(self):
+        try:
+            path_load = str(FileChoose.choose(FileChoose))
+            self.ids.loadDirectory.text = path_load[2:len(path_load)-2]
+            self.status("File loaded.")
+
+        except:
+            self.status("Wrong directory")
+
+    def set_save(self):
+        try:
+            path_save = str(FileChoose.save(FileChoose))
+            self.ids.saveDirectory.text = path_save[2:len(path_save)-2]
+
+            self.status("Saving directory loaded.")
+        except:
+            self.status("Error when choosing a path to save")
+
+    def encrypt(self,keyphrase,plaintext,enc):
+
+            if plaintext.text:
+                try:
+                    ciphertext = binread.handle_input_enc(enc, plaintext.text, keyphrase.text)
+                    self.ids.output.text = ciphertext
+                except:
+                    self.status("Wrong input or keyspace. Check Help")
+
+            if self.ids.loadDirectory.text:
+                try:
+                    fileEncrypt = binread.saving_encrypting(enc,self.ids.loadDirectory.text, self.ids.saveDirectory.text,
+                                                    keyphrase.text)
+                    self.status("Encryption done.")
+                except:
+                    self.status("Wrong input or keyspace. Check Help")
+
+
+
+    def decrypt(self, keyphrase, plaintext,enc):
+
+        if plaintext.text:
+            try:
+                plaintext = binread.handle_input_dec(enc,plaintext.text,keyphrase.text)
+                self.ids.output.text = plaintext
+            except:
+                self.status("Wrong input or keyspace. Check Help")
+
+
+        if self.ids.loadDirectory.text:
+            try:
+                fileDecrypt = binread.reading_decrypting(enc,self.ids.loadDirectory.text, self.ids.saveDirectory.text,
+                                                    keyphrase.text)
+                self.status("Decryption done.")
+            except:
+                self.status("Wrong input or keyspace. Check Help")
+
+
 class Main_page(Screen):
     Window.size = (800, 900)
     Window.clearcolor = (1, 1, 1, 1)
 
+class Atbash_Page(Page):
+    pass
 
-class Caesar_Page(Screen):
-    def refresh(self):
-        self.ids.directory.text = ""
-        self.ids.keyphrase.text = "Place for a key"
-        self.ids.output.text = "Encrypted/Decrypted file"
-        self.ids.plaintext.text = "Place holder for file"
-        self.ids.status.text = ""
+class Vigenere_Page(Page):
+    pass
 
+class Caesar_Page(Page):
+    pass
 
-    def set(self, *args):
-        data = str(FileChoose.choose(FileChoose)).strip().strip("[").strip("]").strip("'")
-        self.ids.directory.text = data
+class Substitution_Page(Page):
+    pass
 
-    def encrypt(self,keyphrase,plaintext):
-        try:
-            ciphertext = cezar.enc_ceasar(int(keyphrase.text),plaintext.text)
-            self.ids.output.text = ciphertext
-            Caesar_Page.status(self, "Encryption done.")
-        except:
-            Caesar_Page.status(self, "Key must be a number between 1-26!")
-            return None
-
-    def decrypt(self, keyphrase, plaintext):
-
-        try:
-            deciphertext = cezar.dec_ceasar(int(keyphrase.text),plaintext.text)
-            self.ids.output.text = deciphertext
-            Caesar_Page.status(self, "Decryption done.")
-        except:
-            Caesar_Page.status(self, "Key must be a number between 1-26!")
-            return None
-
-    def status(self,status):
-        self.ids.status.text = status
-
-class Atbash_Page(Screen):
-
-    def refresh(self):
-        self.ids.directory.text = ""
-        self.ids.output.text = "Encrypted/Decrypted file"
-        self.ids.plaintext.text = "Place holder for file"
-        self.ids.status.text = ""
-
-    def set(self, *args):
-        data = str(FileChoose.choose(FileChoose)).strip().strip("[").strip("]").strip("'")
-        self.ids.directory.text = data
-
-    def encrypt(self,plaintext):
-        try:
-            ciphertext = atbash.toAtBash(plaintext.text)
-            self.ids.output.text = ciphertext
-            Atbash_Page.status(self, "Algorithm done.")
-        except:
-            Atbash_Page.status(self, "Something gone wrong")
-            return None
-
-    def status(self,status):
-        self.ids.status.text = status
-
-
-
-class Vigenere_Page(Screen):
-    def refresh(self):
-        self.ids.directory.text = ""
-        self.ids.keyphrase.text = "Place for a key"
-        self.ids.output.text = "Encrypted/Decrypted file"
-        self.ids.plaintext.text = "Place holder for file"
-        self.ids.status.text = ""
-
-    def set(self, *args):
-        data = str(FileChoose.choose(FileChoose)).strip().strip("[").strip("]").strip("'")
-        self.ids.directory.text = data
-
-    def encrypt(self, keyphrase, plaintext):
-        try:
-            ciphertext = vigenere.encryption(keyphrase.text, plaintext.text)
-            self.ids.output.text = ciphertext
-            Vigenere_Page.status(self, "Encryption done.")
-        except:
-            Vigenere_Page.status(self, "Something gone wrong.")
-
-
-    def decrypt(self, keyphrase, plaintext):
-        try:
-            deciphertext = vigenere.decryption(keyphrase.text, plaintext.text)
-            self.ids.output.text = deciphertext
-            Vigenere_Page.status(self, "Decryption done.")
-        except:
-            Vigenere_Page.status(self, "Something gone wrong.")
-
-    def status(self,status):
-        self.ids.status.text = status
-
-class Substitution_Page(Screen):
-    def refresh(self):
-        self.ids.directory.text = ""
-        self.ids.keyphrase.text = "Place for a key"
-        self.ids.output.text = "Encrypted/Decrypted file"
-        self.ids.plaintext.text = "Place holder for file"
-        self.ids.status.text = ""
-
-    def set(self, *args):
-        data = str(FileChoose.choose(FileChoose)).strip().strip("[").strip("]").strip("'")
-        self.ids.directory.text = data
-
-    def encrypt(self, keyphrase, plaintext):
-        try:
-            ciphertext = substitution.enc_substit(keyphrase.text, plaintext.text)
-            self.ids.output.text = ciphertext
-            Substitution_Page.status(self, "Encryption done.")
-        except:
-            Substitution_Page.status(self, "Something gone wrong.")
-
-    def decrypt(self, keyphrase, plaintext):
-        try:
-            deciphertext = substitution.dec_substit(keyphrase.text, plaintext.text)
-            self.ids.output.text = deciphertext
-            Substitution_Page.status(self, "Decryption done.")
-        except:
-            Substitution_Page.status(self, "Something gone wrong.")
-
-
-    def status(self,status):
-        self.ids.status.text = status
-class Vernam_Page(Screen):
+class Vernam_Page(Page):
     pass
 
 
