7a635cd31177a967f2dc2903b83ac4f518398a4f
IgorMielczarek <100940274+BaldwinSecundo@users.noreply.github.com>
2022-12-29 22:35:03 +0100

    Update cezar.py

--- algorithms/ciphers/cezar.py
+++ algorithms/ciphers/cezar.py
@@ -1,6 +1,3 @@
-import base64
-import binascii
-import string
 
 def enc_ceasar(n, plaintext):
 
@@ -31,47 +28,3 @@ def change(n):
 
 
 
-
-
-
-
-
-# with open("8.jfif", "rb") as reader:
-#     crypted_bytes =[]
-#     non_crypted_bytes =[]
-#     numbers =[]
-#
-#     while (byte := reader.read(1)):
-#         non_crypted_bytes.append(bytes.hex(byte))
-#
-#     with open("9.jfif", "w") as writer:
-#         for x in non_crypted_bytes:
-#
-#             numbers.append(change(x))
-#
-#             crypted_bytes.append(enc_ceasar(42,change(x)))
-#             # writer.write(enc_ceasar(42,x))
-#
-# reader.close()
-# writer.close()
-#
-# print(crypted_bytes)
-
-# for x in range(len(crypted_bytes)):
-#     print(f"{crypted_bytes[x]} || {non_crypted_bytes[x]}")
-
-# sending_bytes =[]
-# with open("9.jfif", "r") as reader:
-#     while (byte := reader.read(2)):
-#         print(f"{byte} ||")
-#         sending_bytes.append(byte)
-# #
-#     with open("10.jfif", "wb") as writer:
-#         for x in sending_bytes:
-#             x = dec_ceasar(42,x)
-#             writer.write(binascii.unhexlify(x))
-
-
-
-# reader.close()
-# writer.close()
