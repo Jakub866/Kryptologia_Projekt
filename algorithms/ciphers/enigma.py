20d7567b8babfd00762fb294867f79829a480d58
Jakub Grzesiak <grzesiakjakub19@gmail.com>
2023-01-01 20:57:36 +0100

    Chagned the binread.py file added support for enigma.py.

--- algorithms/ciphers/enigma.py
+++ algorithms/ciphers/enigma.py
@@ -14,19 +14,19 @@ print(alphabet)
 class Keyboard:
 
     def Forward(self, letter):
-        signal = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".find(letter)
+        signal = "".join((chr(i) for i in range(128))).find(letter)
         return signal
 
     def backward(self, signal):
-        letter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[signal]
+        letter = "".join((chr(i) for i in range(128)))[signal]
         return letter
 
 
 class Plugboard:
 
     def __init__(self, pairs):
-        self.left = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        self.right = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        self.left = "".join((chr(i) for i in range(128)))
+        self.right = "".join((chr(i) for i in range(128)))
 
         for pair in pairs:
             A = pair[0]
@@ -50,7 +50,7 @@ class Plugboard:
 class Rotor:
 
     def __init__(self, wiring, notch):
-        self.left = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        self.left = "".join((chr(i) for i in range(128)))
         self.right = wiring
         self.notch = notch
 
@@ -69,30 +69,29 @@ class Rotor:
         print(self.right)
         print("")
 
-    def rotate(self, n=1, forward = True):
+    def rotate(self, n=1, forward=True):
         for i in range(n):
             if forward:
                 self.left = self.left[1:] + self.left[0]
                 self.right = self.right[1:] + self.right[0]
             else:
-                self.left = self.left[25] + self.left[:25]
-                self.right = self.right[25] + self.right[:25]
-
+                self.left = self.left[127] + self.left[:127]
+                self.right = self.right[127] + self.right[:127]
 
     def rotate_to_letter(self, letter):
-        n = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".find(letter)
+        n = "".join((chr(i) for i in range(128))).find(letter)
         self.rotate(n)
 
     def set_ring(self, n):
-        self.rotate(n-1, False)
-        n_notch = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".find(self.notch)
-        self.notch = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[(n_notch - n) % 26]
+        self.rotate(n - 1, False)
+        n_notch = "".join((chr(i) for i in range(128))).find(self.notch)
+        self.notch = "".join((chr(i) for i in range(128)))[(n_notch - n) % 128]
 
 
 class Reflector:
 
     def __init__(self, wiring):
-        self.left = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        self.left = "".join((chr(i) for i in range(128)))
         self.right = wiring
 
     def reflect(self, signal):
@@ -145,35 +144,49 @@ class Enigma:
         letter = self.kb.backward(signal)
         return letter
 
-
     def set_rings(self, rings):
         self.r1.set_ring(rings[0])
         self.r2.set_ring(rings[1])
         self.r3.set_ring(rings[2])
 
-I = Rotor("EKMFLGDQVZNTOWYHXUSPAIBRCJ", "Q")
 
-II = Rotor("AJDKSIRUXBLHWTMCQGZNPYFVOE", "E")
+index_key = "".join((chr(i) for i in range(65)))
+temp = "".join((chr(i) for i in range(91, 128)))
 
-III = Rotor("BDFHJLCPRTXVZNYEIWGAKMUSQO", "V")
+FirstRotor = index_key + "EKMFLGDQVZNTOWYHXUSPAIBRCJ" + temp
+SecondRotor = index_key + "EKMFLGDQVZNTOWYHXUSPAIBRCJ" + temp
+ThirdRotor = index_key + "EKMFLGDQVZNTOWYHXUSPAIBRCJ" + temp
+FourthRotor = index_key + "EKMFLGDQVZNTOWYHXUSPAIBRCJ" + temp
+FifthRotor = index_key + "VZBRGITYUPSDNHLXAWMJQOFECK" + temp
 
-IV = Rotor("ESOVPZJAYQUIRHXLNFTGKDCMWB", "J")
+ReflectorA = index_key + "EJMZALYXVBWFCRQUONTSPIKHGD" + temp
+ReflectorB = index_key + "YRUHQSLDPXNGOKMIEBFZCWVJAT" + temp
+ReflectorC = index_key + "FVPJIAOYEDRZXWGCTKUQSBNMHL" + temp
 
-V = Rotor("VZBRGITYUPSDNHLXAWMJQOFECK", "Z")
+I = Rotor(FirstRotor, "Q")
 
-A = Reflector("EJMZALYXVBWFCRQUONTSPIKHGD")
-B = Reflector("YRUHQSLDPXNGOKMIEBFZCWVJAT")
-C = Reflector("FVPJIAOYEDRZXWGCTKUQSBNMHL")
+II = Rotor(SecondRotor, "E")
+
+III = Rotor(ThirdRotor, "V")
+
+IV = Rotor(FourthRotor, "J")
+
+V = Rotor(FifthRotor, "Z")
+
+A = Reflector(ReflectorA)
+B = Reflector(ReflectorB)
+C = Reflector(ReflectorC)
 KB = Keyboard()
-PG = Plugboard(["AB", "CD", "EF"])
+PG = Plugboard(["AZ", "BY", "CX"])
 
-ENIGMA = Enigma(B, IV, II, I, PG, KB)
+"""
+ENIGMA = Enigma(A, III, II, I, PG, KB)
 
-ENIGMA.set_key("CAT")
+ENIGMA.set_key("MUL")
 
-ENIGMA.set_rings((5,26,2))
+ENIGMA.set_rings((12, 32, 124))
 
-message = "IEOVGEMYSRHFDICIB"
+message = "TEST"
 message = message.upper()
 
 cipher_text = ""
@@ -182,3 +195,4 @@ for letter in message:
     cipher_text = cipher_text + ENIGMA.encipher(letter)
 
 print(cipher_text)
+"""
--- binread.py
+++ binread.py
@@ -3,12 +3,74 @@ import algorithms.ciphers.cezar as cz
 import algorithms.ciphers.atbash as atbasz
 import algorithms.ciphers.vigenere as vig
 import algorithms.ciphers.substitution as sub
+import algorithms.ciphers.enigma as en
 import time
-
 
 index_key = "".join((chr(i) for i in range(128)))
 
-def saving_encrypting(choose_encryption_method: int, src_f: str, src_s:str, key):
+
+def enigma_dec_enc(Reflector: int, LRotor: int, MRotor: int, RRotor: int, PlugBoard: list, key_set: str,
+                   ring_list: tuple, InOtTx: str):
+    global CLRotor, CMRotor, CRRotor, CReflector
+    match LRotor:
+        case 1:
+            CLRotor = en.I
+        case 2:
+            CLRotor = en.II
+        case 3:
+            CLRotor = en.III
+        case 4:
+            CLRotor = en.IV
+        case 5:
+            CLRotor = en.V
+
+    match MRotor:
+        case 1:
+            CMRotor = en.I
+        case 2:
+            CMRotor = en.II
+        case 3:
+            CMRotor = en.III
+        case 4:
+            CMRotor = en.IV
+        case 5:
+            CMRotor = en.V
+
+    match RRotor:
+        case 1:
+            CRRotor = en.I
+        case 2:
+            CRRotor = en.II
+        case 3:
+            CRRotor = en.III
+        case 4:
+            CRRotor = en.IV
+        case 5:
+            CRRotor = en.V
+
+    match Reflector:
+        case 1:
+            CReflector = en.A
+        case 2:
+            CReflector = en.B
+        case 3:
+            CReflector = en.C
+
+    PG = en.Plugboard(PlugBoard)
+
+    ENIGMA = en.Enigma(CReflector, CLRotor, CMRotor, CRRotor, PG, en.KB)
+    ENIGMA.set_key(key_set)
+    ENIGMA.set_rings(ring_list)
+
+    output = ""
+
+    for letter in InOtTx:
+        output = output + ENIGMA.encipher(letter)
+
+    return output
+
+
+def saving_encrypting(choose_encryption_method: int, src_f: str, src_s: str, key, enigma_key = None):
     OriginalFile = open(src_f, "rb")
     EncryptedFile = open(src_s, "w")
     while (byte := OriginalFile.read(1)):
@@ -16,13 +78,18 @@ def saving_encrypting(choose_encryption_method: int, src_f: str, src_s:str, key)
         for non_crypted_single_bytes in non_crypted_double_bytes:
             match choose_encryption_method:
                 case 1:
-                    EncryptedFile.write(str(index_key.index(cz.enc_ceasar(int(key),non_crypted_single_bytes))))
+                    EncryptedFile.write(str(index_key.index(cz.enc_ceasar(int(key), non_crypted_single_bytes))))
                 case 2:
                     EncryptedFile.write(str(index_key.index(atbasz.toAtBash(non_crypted_single_bytes))))
                 case 3:
-                    EncryptedFile.write((str(index_key.index(vig.encryption(key,non_crypted_single_bytes)))))
+                    EncryptedFile.write((str(index_key.index(vig.encryption(key, non_crypted_single_bytes)))))
                 case 4:
-                    EncryptedFile.write(str(index_key.index(sub.enc_substit(key,non_crypted_single_bytes))))
+                    EncryptedFile.write(str(index_key.index(sub.enc_substit(key, non_crypted_single_bytes))))
+                case 5:
+                    EncryptedFile.write(str(index_key.index(
+                        enigma_dec_enc(enigma_key[0], enigma_key[1], enigma_key[2], enigma_key[3], enigma_key[4],
+                                       enigma_key[5], enigma_key[6],
+                                       non_crypted_single_bytes))))  # Przykład enigma_key = [1, 5, 4, 3, ["AB", "BC", "CD"], "MUL", (12, 32, 124)]
                 case default:
                     pass
             EncryptedFile.write("\n")
@@ -30,24 +97,26 @@ def saving_encrypting(choose_encryption_method: int, src_f: str, src_s:str, key)
     EncryptedFile.close()
 
 
-
-def reading_decrypting(choose_encryption_method: int, src_s: str, src_f:str, key: int):
+def reading_decrypting(choose_encryption_method: int, src_s: str, src_f: str, key: int, enigma_key=None):
     time.sleep(3)
     EncrypredFile = open(src_s, "r")
     DecryptedFile = open(src_f, "wb")
     wynik = ""
     while (byte := EncrypredFile.readline()):
         byte = byte.strip()
-        byte =  index_key[int(byte)]
+        byte = index_key[int(byte)]
         match choose_encryption_method:
             case 1:
                 byte = cz.dec_ceasar(int(key), byte)
             case 2:
                 byte = atbasz.toAtBash(byte)
             case 3:
-                byte = vig.decryption(key,byte)
+                byte = vig.decryption(key, byte)
             case 4:
-                byte = sub.dec_substit(key,byte)
+                byte = sub.dec_substit(key, byte)
+            case 5:
+                byte = enigma_dec_enc(enigma_key[0], enigma_key[1], enigma_key[2], enigma_key[3], enigma_key[4],
+                                      enigma_key[5], enigma_key[6], byte)
             case default:
                 pass
         wynik = wynik + byte
@@ -57,28 +126,37 @@ def reading_decrypting(choose_encryption_method: int, src_s: str, src_f:str, key
     DecryptedFile.close()
     EncrypredFile.close()
 
-def handle_input_enc(choose_encryption_method: int, inp, key):
+
+def handle_input_enc(choose_encryption_method: int, inp, key, enigma_key=None):
     match choose_encryption_method:
         case 1:
-            return cz.enc_ceasar(int(key),inp)
+            return cz.enc_ceasar(int(key), inp)
         case 2:
             return atbasz.toAtBash(inp)
         case 3:
-            return vig.encryption(key,inp)
+            return vig.encryption(key, inp)
         case 4:
-            return sub.enc_substit(key,inp)
+            return sub.enc_substit(key, inp)
+        case 5:
+            return enigma_dec_enc(enigma_key[0], enigma_key[1], enigma_key[2], enigma_key[3], enigma_key[4],
+                                  enigma_key[5], enigma_key[6], inp)
         case default:
             pass
 
-def handle_input_dec(choose_encryption_method: int, inp, key):
+
+
+def handle_input_dec(choose_encryption_method: int, inp, key, enigma_key=None):
     match choose_encryption_method:
         case 1:
-            return cz.dec_ceasar(int(key),inp)
+            return cz.dec_ceasar(int(key), inp)
         case 2:
             return atbasz.toAtBash(inp)
         case 3:
-            return vig.decryption(key,inp)
+            return vig.decryption(key, inp)
         case 4:
-            return sub.dec_substit(key,inp)
+            return sub.dec_substit(key, inp)
+        case 5:
+            return enigma_dec_enc(enigma_key[0], enigma_key[1], enigma_key[2], enigma_key[3], enigma_key[4],
+                                  enigma_key[5], enigma_key[6], inp)
         case default:
             pass
