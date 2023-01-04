084ade60f066b0b0babe116a5513e0df1f40f1bf
Jakub Grzesiak <grzesiakjakub19@gmail.com>
2023-01-04 20:00:08 +0100

    Zmieniono plik enigma.py, zostało wprowadzone wsparcie dla systemu znaków ASCII. Zmieniono implementację funkcji enigma_dec_enc() w pliku binread.py aby wspierała zapisywanie z plików oraz tworzenie nowych instacji rotorów przy nowym wywołaniu funkcji co pozwala na bezproblemowe działanie tej funkcji.

--- binread.py
+++ binread.py
@@ -9,52 +9,46 @@ import time
 index_key = "".join((chr(i) for i in range(128)))
 
 
-def enigma_dec_enc(Reflector: int, LRotor: int, MRotor: int, RRotor: int, PlugBoard: list, key_set: str,
+def enigma_dec_enc(LRotor: int, MRotor: int, RRotor: int, PlugBoard: list, key_set: str,
                    ring_list: tuple, InOtTx: str):
     global CLRotor, CMRotor, CRRotor, CReflector
     match LRotor:
         case 1:
-            CLRotor = en.I
+            CLRotor = en.Rotor(en.SRotorA, "Q")
         case 2:
-            CLRotor = en.II
+            CLRotor = en.Rotor(en.SRotorB, "E")
         case 3:
-            CLRotor = en.III
+            CLRotor = en.Rotor(en.SRotorC, "V")
         case 4:
-            CLRotor = en.IV
+            CLRotor = en.Rotor(en.SRotorE, "J")
         case 5:
-            CLRotor = en.V
+            CLRotor = en.Rotor(en.SRotorF, "Z")
 
     match MRotor:
         case 1:
-            CMRotor = en.I
+            CMRotor = en.Rotor(en.SRotorA, "Q")
         case 2:
-            CMRotor = en.II
+            CMRotor = en.Rotor(en.SRotorB, "E")
         case 3:
-            CMRotor = en.III
+            CMRotor = en.Rotor(en.SRotorC, "V")
         case 4:
-            CMRotor = en.IV
+            CMRotor = en.Rotor(en.SRotorE, "J")
         case 5:
-            CMRotor = en.V
+            CMRotor = en.Rotor(en.SRotorF, "Z")
 
     match RRotor:
         case 1:
-            CRRotor = en.I
+            CRRotor = en.Rotor(en.SRotorA, "Q")
         case 2:
-            CRRotor = en.II
+            CRRotor = en.Rotor(en.SRotorB, "E")
         case 3:
-            CRRotor = en.III
+            CRRotor = en.Rotor(en.SRotorC, "V")
         case 4:
-            CRRotor = en.IV
+            CRRotor = en.Rotor(en.SRotorE, "J")
         case 5:
-            CRRotor = en.V
+            CRRotor = en.Rotor(en.SRotorF, "Z")
 
-    match Reflector:
-        case 1:
-            CReflector = en.A
-        case 2:
-            CReflector = en.B
-        case 3:
-            CReflector = en.C
+    CReflector = en.Reflector(en.SReflector)
 
     PG = en.Plugboard(PlugBoard)
 
@@ -70,7 +64,7 @@ def enigma_dec_enc(Reflector: int, LRotor: int, MRotor: int, RRotor: int, PlugBo
     return output
 
 
-def saving_encrypting(choose_encryption_method: int, src_f: str, src_s: str, key, enigma_key = None):
+def saving_encrypting(choose_encryption_method: int, src_f: str, src_s: str, key, enigma_key=None):
     OriginalFile = open(src_f, "rb")
     EncryptedFile = open(src_s, "w")
     while (byte := OriginalFile.read(1)):
@@ -87,8 +81,8 @@ def saving_encrypting(choose_encryption_method: int, src_f: str, src_s: str, key
                     EncryptedFile.write(str(index_key.index(sub.enc_substit(key, non_crypted_single_bytes))))
                 case 5:
                     EncryptedFile.write(str(index_key.index(
-                        enigma_dec_enc(enigma_key[0], enigma_key[1], enigma_key[2], enigma_key[3], enigma_key[4],
-                                       enigma_key[5], enigma_key[6],
+                        enigma_dec_enc(enigma_key[0], enigma_key[1], enigma_key[2], enigma_key[3],
+                                       enigma_key[4], enigma_key[5],
                                        non_crypted_single_bytes))))  # Przykład enigma_key = [1, 5, 4, 3, ["AB", "BC", "CD"], "MUL", (12, 32, 124)]
                 case default:
                     pass
@@ -97,7 +91,7 @@ def saving_encrypting(choose_encryption_method: int, src_f: str, src_s: str, key
     EncryptedFile.close()
 
 
-def reading_decrypting(choose_encryption_method: int, src_s: str, src_f: str, key: int, enigma_key=None):
+def reading_decrypting(choose_encryption_method: int, src_s: str, src_f: str, key, enigma_key=None):
     time.sleep(3)
     EncrypredFile = open(src_s, "r")
     DecryptedFile = open(src_f, "wb")
@@ -115,8 +109,9 @@ def reading_decrypting(choose_encryption_method: int, src_s: str, src_f: str, ke
             case 4:
                 byte = sub.dec_substit(key, byte)
             case 5:
-                byte = enigma_dec_enc(enigma_key[0], enigma_key[1], enigma_key[2], enigma_key[3], enigma_key[4],
-                                      enigma_key[5], enigma_key[6], byte)
+                byte = enigma_dec_enc(enigma_key[0], enigma_key[1], enigma_key[2], enigma_key[3],
+                                      enigma_key[4], enigma_key[5],
+                                      byte)
             case default:
                 pass
         wynik = wynik + byte
@@ -138,11 +133,11 @@ def handle_input_enc(choose_encryption_method: int, inp, key, enigma_key=None):
         case 4:
             return sub.enc_substit(key, inp)
         case 5:
-            return enigma_dec_enc(enigma_key[0], enigma_key[1], enigma_key[2], enigma_key[3], enigma_key[4],
-                                  enigma_key[5], enigma_key[6], inp)
+            return enigma_dec_enc(enigma_key[0], enigma_key[1], enigma_key[2], enigma_key[3],
+                                  enigma_key[4], enigma_key[5],
+                                  inp)
         case default:
             pass
-
 
 
 def handle_input_dec(choose_encryption_method: int, inp, key, enigma_key=None):
@@ -156,7 +151,18 @@ def handle_input_dec(choose_encryption_method: int, inp, key, enigma_key=None):
         case 4:
             return sub.dec_substit(key, inp)
         case 5:
-            return enigma_dec_enc(enigma_key[0], enigma_key[1], enigma_key[2], enigma_key[3], enigma_key[4],
-                                  enigma_key[5], enigma_key[6], inp)
+            return enigma_dec_enc(enigma_key[0], enigma_key[1], enigma_key[2], enigma_key[3],
+                                  enigma_key[4], enigma_key[5],
+                                  inp)
         case default:
             pass
+
+# Przykład enigma_key = [5, 4, 3, ["AB", "BC", "CD"], "MUL", (12, 32, 124)]
+#saving_encrypting(5, "README.md", "ENREADME.MD", None, [3, 2, 1, ["AR", "XO", "EW"], "123", (2, 4, 59)])
+#reading_decrypting(5, "ENREADME.MD", "CHECK.MD", None, [3, 2, 1, ["AR", "XO", "EW"], "123", (2, 4, 59)])
+
+#Pierwsze 3 liczby to wybór rotora od lewej do prawej możliwy wybór: 1-5
+#Kolejnym jest lista dwóch znaków oznaczająca pluggboarda, tj jaka z liter zamieniana jest z jaką.
+#3 literowy string to wybór od jakich liter/znaków zaczynają się rotory.
+#Krotka zawierająca 3 liczby to numer który odpowiada za to w którym momencie rotor zostanie poruszony w kolejną pozycję dodaję dużą ilość złożności.
+#UWAGA nie ma możliwości wyboru reflektora. jest tylko jeden.
