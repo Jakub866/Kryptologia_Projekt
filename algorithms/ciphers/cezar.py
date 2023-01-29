cd3adf582c3697a800405a7414e308730cd366c3
Jakub Grzesiak <34369682+Jakub866@users.noreply.github.com>
2023-01-29 12:26:02 +0100

    Update cezar.py

--- algorithms/ciphers/cezar.py
+++ algorithms/ciphers/cezar.py
@@ -15,16 +15,3 @@ def dec_ceasar(n,cyphertext):
             i = (key.index(l) - n) % 128
             result += key[i]
     return result
-
-def change(n):
-    key = ''.join(chr(i) for i in range(128))
-    key = [i for i in key]
-    result = ""
-    for l in n:
-        for x in range(len(key)):
-            if key[x] == l:
-                result += str(x) + ","
-    return result
-
-
-
